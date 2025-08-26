401c3636c112aaea3e6b0cac7fd41cf7303cf13d
Yi Xin Tan <75169586+YiXin-Tan@users.noreply.github.com>
2025-08-26 18:29:05 +1000

    Added MQTT comms

--- src/rotary_mqtt.py
+++ src/rotary_mqtt.py
@@ -1,4 +1,8 @@
 from gpiozero import RotaryEncoder, Button
+import paho.mqtt.client as mqtt
+
+mqttc = mqtt.Client()
+mqttc.connect("localhost", 1883, 60)
 
 encoder = RotaryEncoder(a=17, b=18, max_steps=100)
 button = Button(27)
@@ -6,12 +10,16 @@ button = Button(27)
 def rotate():
     if encoder.steps > 0:
         print("CW")
+        mqttc.publish("home/rotary/light", "brightness_up")
     else:
         print("CCW")
+        mqttc.publish("home/rotary/light", "brightness_down")
 
 def press():
     print("Button pressed")
+    mqttc.publish("home/rotary/light", "toggle")
 
 encoder.when_rotated = rotate
 button.when_pressed = press
 
+mqttc.loop_forever()
